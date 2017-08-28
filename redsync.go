917337156325b1b9ecd21e09b17a1820dcc21dab
Wyatt Johnson <wyattjoh@gmail.com>
2017-08-28 14:36:35 -0600

    removed old WithValue option

--- redsync.go
+++ redsync.go
@@ -71,10 +71,3 @@ func SetDriftFactor(factor float64) Option {
 		m.factor = factor
 	})
 }
-
-// SetValue sets the value of the mutex.
-func SetValue(value string) Option {
-	return OptionFunc(func(m *Mutex) {
-		m.value = value
-	})
-}
