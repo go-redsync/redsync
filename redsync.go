a6bfc68dd06c0b854da25c6384e87e8937707067
mehdijoafshani <mahdi.joafshani@gmail.com>
2021-04-14 13:14:46 +0430

    change min/max retry delay

--- redsync.go
+++ redsync.go
@@ -7,6 +7,11 @@ import (
 	"github.com/go-redsync/redsync/v4/redis"
 )
 
+const (
+	minRetryDelayMilliSec = 50
+	maxRetryDelayMilliSec = 250
+)
+
 // Redsync provides a simple method for creating distributed mutexes using multiple Redis connection pools.
 type Redsync struct {
 	pools []redis.Pool
@@ -22,10 +27,12 @@ func New(pools ...redis.Pool) *Redsync {
 // NewMutex returns a new distributed mutex with given name.
 func (r *Redsync) NewMutex(name string, options ...Option) *Mutex {
 	m := &Mutex{
-		name:         name,
-		expiry:       8 * time.Second,
-		tries:        32,
-		delayFunc:    func(tries int) time.Duration { return time.Duration(rand.Intn(500-100)+100) * time.Millisecond },
+		name:   name,
+		expiry: 8 * time.Second,
+		tries:  32,
+		delayFunc: func(tries int) time.Duration {
+			return time.Duration(rand.Intn(maxRetryDelayMilliSec-minRetryDelayMilliSec)+minRetryDelayMilliSec) * time.Millisecond
+		},
 		genValueFunc: genValue,
 		factor:       0.01,
 		quorum:       len(r.pools)/2 + 1,
