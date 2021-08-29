7b4b123b58adfa6a165858eca94f970aa751adae
Mahmud Ridwan <m@hjr265.me>
2021-08-29 23:18:31 +0600

    Add Godoc for ErrExtendFailed

--- error.go
+++ error.go
@@ -5,4 +5,7 @@ import "errors"
 // ErrFailed is the error resulting if Redsync fails to acquire the lock afer
 // exhausting all retries.
 var ErrFailed = errors.New("redsync: failed to acquire lock")
+
+// ErrExtendFailed is the error resulting if Redsync fails to extend the
+// lock.
 var ErrExtendFailed = errors.New("redsync: failed to extend lock")
