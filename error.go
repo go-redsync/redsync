81c4ce0ee75ef60922085b723fa7b68f2cac9594
Xabier <xabier.martinez@cabify.com>
2022-12-02 10:20:57 +0100

    Add node information in error

--- error.go
+++ error.go
@@ -1,6 +1,9 @@
 package redsync
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+)
 
 // ErrFailed is the error resulting if Redsync fails to acquire the lock after
 // exhausting all retries.
@@ -9,3 +12,32 @@ var ErrFailed = errors.New("redsync: failed to acquire lock")
 // ErrExtendFailed is the error resulting if Redsync fails to extend the
 // lock.
 var ErrExtendFailed = errors.New("redsync: failed to extend lock")
+
+// ErrTaken happens when the lock is already taken in a quorum on nodes.
+type ErrTaken struct {
+	Nodes []int
+}
+
+func (err ErrTaken) Error() string {
+	return fmt.Sprintf("lock already taken, locked nodes: %v", err.Nodes)
+}
+
+// ErrNodeTaken is the error resulting if the lock is already taken in one of
+// the cluster's nodes
+type ErrNodeTaken struct {
+	Node int
+}
+
+func (err ErrNodeTaken) Error() string {
+	return fmt.Sprintf("node #%d: lock already taken", err.Node)
+}
+
+// A RedisError is an error communicating with one of the Redis nodes.
+type RedisError struct {
+	Node int
+	Err  error
+}
+
+func (err RedisError) Error() string {
+	return fmt.Sprintf("node #%d: %s", err.Node, err.Err)
+}
