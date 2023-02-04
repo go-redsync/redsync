591e767ebc2ef79d03353a622e5d94b2839d2048
Artem Utkin <solid.utkin@gmail.com>
2023-02-04 12:37:45 +0300

    update go-redis to v9.0.2

---
+++ examples/goredis/v9/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"context"
+
+	"github.com/go-redsync/redsync/v4"
+	"github.com/go-redsync/redsync/v4/redis/goredis/v9"
+	goredislib "github.com/redis/go-redis/v9"
+	"github.com/stvp/tempredis"
+)
+
+func main() {
+	server, err := tempredis.Start(tempredis.Config{})
+	if err != nil {
+		panic(err)
+	}
+	defer server.Term()
+
+	client := goredislib.NewClient(&goredislib.Options{
+		Network: "unix",
+		Addr:    server.Socket(),
+	})
+
+	pool := goredis.NewPool(client)
+
+	rs := redsync.New(pool)
+
+	mutex := rs.NewMutex("test-redsync")
+	ctx := context.Background()
+
+	if err := mutex.LockContext(ctx); err != nil {
+		panic(err)
+	}
+
+	if _, err := mutex.UnlockContext(ctx); err != nil {
+		panic(err)
+	}
+}
